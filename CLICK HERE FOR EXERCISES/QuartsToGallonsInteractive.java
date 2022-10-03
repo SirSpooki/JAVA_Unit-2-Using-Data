a84d1e9c488aaab09bcdc0a1891a1b7253b4fce8
SirSpooki <114715053+SirSpooki@users.noreply.github.com>
2022-10-03 11:22:44 -0500

    Add files via upload

---
+++ CLICK HERE FOR EXERCISES/QuartsToGallonsInteractive.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class QuartsToGallonsInteractive
+
+{
+   public static void main(String[] args) 
+   {   
+      // Modify the code below
+      final int QUARTS_IN_GALLON = 4;
+
+      int quartsNeeded;
+      Scanner inputDevice = new Scanner(System.in);
+      System.out.println("Enter quarts needed");
+      quartsNeeded = inputDevice.nextInt();
+      
+      int gallonsNeeded;
+      int extraQuartsNeeded;
+      gallonsNeeded = quartsNeeded / QUARTS_IN_GALLON;
+      extraQuartsNeeded = quartsNeeded % QUARTS_IN_GALLON;
+      System.out.println("A job that needs " + quartsNeeded +
+         " quarts requires " + gallonsNeeded + " gallons plus " +
+         extraQuartsNeeded + " quarts."); 
+   }
+}
