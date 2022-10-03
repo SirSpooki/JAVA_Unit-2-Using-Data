dfaf0462a63e016a14c64b34f76314848b8429f0
SirSpooki <114715053+SirSpooki@users.noreply.github.com>
2022-10-03 11:32:52 -0500

    Add files via upload

---
+++ CLICK HERE FOR DEBUGGING/DebugTwo3.java
@@ -0,0 +1,14 @@
+public class DebugTwo3
+{
+   public static void main(String[] args)
+   {
+      int a, b, result;
+      long c = 7777777777777L;
+      a = 99;
+      b = 8;
+      result = a % b;
+      System.out.println("Divide " + a + " by " + b);
+      System.out.println("remainder is " + result);
+      System.out.println("c is a very large number: " + c);
+    }
+}
